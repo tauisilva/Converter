81f116c7d922d15ba4181e558a59647c95ff2cb4
Taui Silva <tauisilva@gmail.com>
2024-03-25 20:30:52 -0300

    FIX - varias msg de erro, add loading no reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,10 +35,12 @@ export class AppComponent {
   value: any = null; // Valor de entrada do usuário
   validCharacters: string = ''; // Armazena os caracteres válidos do escopo ASCII
   init: boolean = false; // Indica se o componente foi inicializado
+  refresh: boolean = false; // Atualização dos valores
   visible: boolean = false; // Indica se algum elemento é visível na interface do usuário
   caracInfos = caracteresDeControle; // Informações sobre caracteres de controle
   outrasInfos = informacoes; // Outras informações sobre o componente
   msg: any = null; // Mensagem de erro ou aviso
+  previousInput: string = ''; // Armazena a entrada anterior do usuário
 
   // Injetando os serviços necessários
   private messageService = inject(MessageService);
@@ -46,31 +48,42 @@ export class AppComponent {
 
   // Método chamado quando há uma mudança na entrada do usuário
   onInputChange(value: string) {
+    this.verifyCaracteres(value);
+  }
+
+  verifyCaracteres(value: string) {
     let invalidCharacters = '';
 
-    // Reiniciando a lista de caracteres válidos
-    this.validCharacters = '';
+    // Verificar se houve mudança nos caracteres inválidos
+    if (value !== this.previousInput) {
+      // Reiniciando a lista de caracteres válidos
+      this.validCharacters = '';
 
-    // Verificando cada caractere da entrada do usuário
-    for (let i = 0; i < value.length; i++) {
-      const char = value[i];
-      if (this.isAscii(char)) {
-        this.validCharacters += char; // Concatenando caracteres válidos
-      } else {
-        invalidCharacters += char; // Concatenando caracteres inválidos
+      // Verificando cada caractere da entrada do usuário
+      for (let i = 0; i < value.length; i++) {
+        const char = value[i];
+        if (this.isAscii(char)) {
+          this.validCharacters += char; // Concatenando caracteres válidos
+        } else {
+          invalidCharacters += char; // Concatenando caracteres inválidos
+        }
+      }
+
+      // Se houver caracteres inválidos, exibe uma mensagem de erro
+      if (invalidCharacters !== '') {
+        this.showMessage(
+          'error',
+          'Caracteres Inválidos',
+          `Use apenas caracteres pertencentes à tabela ASCII.`
+        );
+        this.msg = invalidCharacters;
       }
     }
 
-    // Se houver caracteres inválidos, exibe uma mensagem de erro
-    if (invalidCharacters !== '') {
-      this.showMessage(
-        'error',
-        'Caracteres Inválidos',
-        `Use apenas caracteres pertencentes à tabela ASCII.`
-      );
-      this.msg = invalidCharacters;
-    }
+    // Atualizar a entrada anterior
+    this.previousInput = value;
   }
+
 
   // Método para verificar se um caractere é ASCII
   isAscii(char: string): boolean {
@@ -80,43 +93,69 @@ export class AppComponent {
   }
 
   // Método para recarregar o componente
-  reload(event: Event) {
+  isReload() {
+    this.verifyCaracteres(this.value);
+    this.refresh = true;
     this.init = false;
-    this.msg = null;
     setTimeout(() => {
       this.init = true;
+      this.refresh = false;
     }, 500);
   }
 
+  isInit() {
+    this.init = true;
+    this.refresh = false;
+  }
+
   // Método para confirmar uma ação com o usuário
-  confirm(event: Event) {
-    if (this.msg !== null) {
-      this.confirmationService.confirm({
-        target: event.target as EventTarget,
-        message: 'Alguns caracteres foram removidos, continuar?',
-        header: 'Confirmação',
-        icon: 'pi pi-exclamation-triangle',
-        acceptIcon: "none",
-        rejectIcon: "none",
-        rejectButtonStyleClass: "p-button-text",
-        accept: () => {
-          this.init = true;
-        },
-        reject: () => {
-          this.msg = null;
-          this.init = false;
-          this.value = null;
-          this.validCharacters = '';
+  confirm(event: Event, isReload?: boolean) {
+    if (this.validCharacters !== '') {
+      if (this.msg !== null) {
+        this.confirmationService.confirm({
+          target: event.target as EventTarget,
+          message: 'Alguns caracteres foram removidos, continuar?',
+          header: 'Confirmação',
+          icon: 'pi pi-exclamation-triangle',
+          acceptIcon: "none",
+          rejectIcon: "none",
+          rejectButtonStyleClass: "p-button-text",
+          accept: () => {
+            if (isReload) {
+              this.isReload();
+            } else {
+              this.isInit();
+            }
+          },
+          reject: () => {
+            this.msg = null;
+            this.init = false;
+            this.value = null;
+            this.validCharacters = '';
+            this.refresh = false;
+          }
+        });
+      } else {
+        if (isReload) {
+          this.isReload();
+        } else {
+          this.isInit();
         }
-      });
+      }
     } else {
-      this.init = true;
+      this.showMessage('warn', 'Verifique os dados', 'É necessário ao menos um caractere válido e compatível com a ASCII estendida');
+      this.msg = null;
+      this.init = false;
+      this.value = null;
+      this.validCharacters = '';
+      this.refresh = false;
     }
   }
 
   // Método para exibir uma mensagem na interface
   showMessage(sev: string, summ: string, detail: string) {
     this.messageService.add({
+      key: 'bc',
       severity: sev,
       summary: summ,
       detail: detail
