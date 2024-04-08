833f961edda463d2743828b2bad2faa329365c42
Taui Silva <tauisilva@gmail.com>
2024-04-08 00:13:04 -0300

    Style - aplicado new UI, implementado temas dark and light

---
+++ adasdsa.ts
@@ -0,0 +1,181 @@
+import { NgIf } from '@angular/common';
+import { Component, ViewEncapsulation, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ChipModule } from 'primeng/chip';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { DialogModule } from 'primeng/dialog';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TabViewModule } from 'primeng/tabview';
+import { ToastModule } from 'primeng/toast';
+import { caracteresDeControle, informacoes } from './infos/caracteres';
+import { InfosComponent } from './infos/infos.component';
+import { TableComponent } from './table/table.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet, ChipModule, FormsModule,
+    InputTextModule, ButtonModule, TableComponent,
+    NgIf, InfosComponent, ScrollPanelModule,
+    DialogModule, TabViewModule, ToastModule,
+    ConfirmPopupModule, InputSwitchModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  // Propriedades do componente
+  title = 'Converter | ASCII'; // Título da página
+  value: any = null; // Valor de entrada do usuário
+  validCharacters: string = ''; // Armazena os caracteres válidos do escopo ASCII
+  init: boolean = false; // Indica se o componente foi inicializado
+  refresh: boolean = false; // Atualização dos valores
+  visible: boolean = false; // Indica se algum elemento é visível na interface do usuário
+  caracInfos = caracteresDeControle; // Informações sobre caracteres de controle
+  outrasInfos = informacoes; // Outras informações sobre o componente
+  msg: any = null; // Mensagem de erro ou aviso
+  previousInput: string = ''; // Armazena a entrada anterior do usuário
+  isAscExtends: boolean = false; // Modifica entre AscII(entre 0 e 127) e AscII Extends(128-255)
+
+  // links
+  link_table = 'https://www.asciitable.com/;'
+  // links
+
+
+  // Injetando os serviços necessários
+  private messageService = inject(MessageService);
+  private confirmationService = inject(ConfirmationService);
+
+  // Método chamado quando há uma mudança na entrada do usuário
+  onInputChange(value: string) {
+    this.verifyCaracteres(value);
+  }
+
+  verifyCaracteres(value: string) {
+    let invalidCharacters = '';
+
+    // Reiniciando a lista de caracteres válidos
+    this.validCharacters = '';
+
+    // Verificando cada caractere da entrada do usuário
+    for (let i = 0; i < value.length; i++) {
+      const char = value[i];
+      if (this.isAscii(char)) {
+        this.validCharacters += char; // Concatenando caracteres válidos
+      } else {
+        invalidCharacters += char; // Concatenando caracteres inválidos
+      }
+    }
+
+    // Se houver caracteres inválidos, exibe uma mensagem de erro
+    if (invalidCharacters !== '' && this.msg !== invalidCharacters) {
+      this.showMessage(
+        'error',
+        'Caracteres Inválidos',
+        `Use apenas caracteres pertencentes à tabela ASCII.`
+      );
+      this.msg = invalidCharacters;
+    } else {
+      this.msg = null;
+    }
+    // Verificar se houve mudança nos caracteres inválidos
+    if (value !== this.previousInput) {
+    }
+
+    // Atualizar a entrada anterior
+    this.previousInput = value;
+  }
+
+
+  // Método para verificar se um caractere é ASCII
+  isAscii(char: string): boolean {
+    const regexASCII = /^[\x00-\x7F]*$/;
+    const regexASCIIExtended = /^[\x00-\xFF]*$/;
+    return this.isAscExtends ?
+      regexASCIIExtended.test(char) :
+      regexASCII.test(char);
+  }
+
+  // Método para recarregar o componente
+  isReload() {
+    this.verifyCaracteres(this.value);
+    this.refresh = true;
+    this.init = false;
+    setTimeout(() => {
+      this.init = true;
+      this.refresh = false;
+    }, 500);
+  }
+
+  isInit() {
+    this.init = true;
+    this.refresh = false;
+  }
+
+  // Método para confirmar uma ação com o usuário
+  confirm(event: Event, isReload?: boolean) {
+    if (this.validCharacters !== '') {
+      if (this.msg !== null) {
+        this.confirmationService.confirm({
+          target: event.target as EventTarget,
+          message: 'Alguns caracteres foram removidos, continuar?',
+          header: 'Confirmação',
+          icon: 'pi pi-exclamation-triangle',
+          acceptIcon: "none",
+          rejectIcon: "none",
+          rejectButtonStyleClass: "p-button-text",
+          accept: () => {
+            if (isReload) {
+              this.isReload();
+            } else {
+              this.isInit();
+            }
+          },
+          reject: () => {
+            this.msg = null;
+            this.init = false;
+            this.value = null;
+            this.validCharacters = '';
+            this.refresh = false;
+          }
+        });
+      } else {
+        if (isReload) {
+          this.isReload();
+        } else {
+          this.isInit();
+        }
+      }
+    } else {
+      this.showMessage(
+        'warn',
+        'Verifique os dados',
+        'É necessário ao menos um caractere válido e compatível com a ASCII estendida'
+      );
+      this.msg = null;
+      this.init = false;
+      this.value = null;
+      this.validCharacters = '';
+      this.refresh = false;
+    }
+  }
+
+  // Método para exibir uma mensagem na interface
+  showMessage(sev: string, summ: string, detail: string) {
+    this.messageService.add({
+      key: 'bc',
+      severity: sev,
+      summary: summ,
+      detail: detail
+    });
+  }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,131 @@
 import { NgIf } from '@angular/common';
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
 import { ChipModule } from 'primeng/chip';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { DialogModule } from 'primeng/dialog';
+import { InputSwitchModule } from 'primeng/inputswitch';
 import { InputTextModule } from 'primeng/inputtext';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { TabViewModule } from 'primeng/tabview';
-import { caracteresDeControle, informacoes } from './infos/caracteres';
+import { ToastModule } from 'primeng/toast';
+import { TooltipModule } from 'primeng/tooltip';
 import { InfosComponent } from './infos/infos.component';
 import { TableComponent } from './table/table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    ChipModule,
-    FormsModule,
-    InputTextModule,
-    ButtonModule,
-    TableComponent,
-    NgIf,
-    InfosComponent,
-    ScrollPanelModule,
-    DialogModule,
-    TabViewModule
+    RouterOutlet, ChipModule, FormsModule,
+    InputTextModule, ButtonModule, TableComponent,
+    NgIf, InfosComponent, ScrollPanelModule,
+    DialogModule, TabViewModule, ToastModule, TooltipModule,
+    InputSwitchModule, CheckboxModule, ConfirmPopupModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'converter | ASCII'; // Título da aplicação
-  value: any = null; // Valor inicial para a entrada de dados
-  init: boolean = false; // Indica se a aplicação foi inicializada
-  visible: boolean = false; // Indica se um componente é visível na interface
-  caracInfos = caracteresDeControle; // Informações sobre caracteres de controle
-  outrasInfos = informacoes; // Outras informações relevantes
+export class AppComponent implements OnInit {
+  //<-------------- Variveis -------------->
 
-  // Método para recarregar a aplicação
-  // Necessario para limpeza dos dados e evitar uso de lixo
-  reload() {
-    this.init = false; // Define que a aplicação não está inicializada
-    setTimeout(() => {
-      this.init = true; // Define que a aplicação está inicializada após um pequeno intervalo
-    }, 500); // Tempo de espera antes de reinicializar a aplicação (500ms)
+  title = 'Converter | ASCII'; // Título da página
+  isDark: boolean = false; // Controle do tema
+  openModal: boolean = false; // Controle do modal com informações 
+  isAscExtends: boolean = false; // Modifica entre AscII(entre 0 e 127) e AscII Extends(128-255)
+  init: boolean = false; // Indica se o componente foi inicializado
+  refresh: boolean = false; // Atualização dos valores
+  skeleton: { [key: string]: boolean } = {
+    initSkeleton: false,
+    title: false,
+    btns: false,
+    input: false,
+    card: false
+  };
+
+  inputValue: any = null; // Valor de entrada do usuário
+  invalidCharactersStack: any; // Pilha para armazenar caracteres inválidos
+
+  //<-------------- Variveis -------------->
+
+  //<-------------- Links -------------->
+
+  link_table = 'https://www.asciitable.com/;'
+
+  //<-------------- Links -------------->
+
+  async ngOnInit(): Promise<void> {
+    this.ativarSkeleton();
+    let time = 500;
+    Object.keys(this.skeleton).forEach((key: string) => {
+      time += 10;
+      if (key !== 'initSkeleton') {
+        this.disableSkeleton(key, time);
+      }
+    });
+    this.skeleton['initSkeleton'] = false;
+  }
+
+  ativarSkeleton(): void {
+    Object.keys(this.skeleton).forEach((key: string) => {
+      this.skeleton[key] = true;
+    });
+  }
+
+  async disableSkeleton(key: string, time?: number) {
+    await new Promise<void>(resolve =>
+      setTimeout(() => {
+        console.log(key, time)
+        this.skeleton[key] = false;
+        resolve();
+      }, time ? time : 500));
+  }
+
+  async initConverter() {
+    this.init = true;
+    await this.verifyCaracteres(this.inputValue);
+  }
+
+  verifyCaracteres(value: any) {
+    let validCharacters = '';
+    this.invalidCharactersStack = '';
+    for (let i = 0; i < value.length; i++) {
+      const char = value[i];
+      if (this.isAscii(char)) {
+        validCharacters += char; // Concatenando caracteres válidos
+      } else {
+        this.invalidCharactersStack += char // Concatenando caracteres inválidos
+      }
+    }
+  }
+
+  //<-------------- REGEX's -------------->
+
+  // Método para verificar se um caractere é ASCII
+  isAscii(char: string): boolean {
+    const regexASCII = /^[\x00-\x7F]*$/;
+    const regexASCIIExtended = /^[\x00-\xFF]*$/;
+    return this.isAscExtends ?
+      regexASCIIExtended.test(char) :
+      regexASCII.test(char);
+  }
+
+  //<-------------- REGEX's -------------->
+
+  getIcon(theme?: boolean) {
+    if (theme) {
+      return this.isDark ? 'bi-sun' : 'bi-moon-stars';
+    } else {
+      return this.inputValue !== null && this.init ?
+        'bi-arrow-clockwise text-xl' :
+        'bi-play text-3xl';
+    }
+  }
+
+  getTheme() {
+    return this.isDark ? 'dark' : 'light';
   }
 }
